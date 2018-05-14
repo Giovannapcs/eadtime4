a63b4008bf6c5a7b605284a00065d6c169c9945b
AdrianaAguiar <AdrianaAguiar@DESKTOP-60SM39G>
2018-05-14 00:53:12 -0300

    Merge commit '71127a88c2fd1cdcfe2cdc7741ae5d676b947d17'x

--- src/br/iesb/meuprograma/apresentacao/JFramePrincipal.java
+++ src/br/iesb/meuprograma/apresentacao/JFramePrincipal.java
@@ -116,9 +116,9 @@ public class JFramePrincipal extends javax.swing.JFrame {
 
     private void jMenuItem2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem2ActionPerformed
         // TODO add your handling code here:
-        FrmPesquisarProcesso obj = new FrmPesquisarProcesso();
+        //FrmPesquisarProcesso obj = new FrmPesquisarProcesso();
        
-          obj.setVisible(true);
+          //obj.setVisible(true);
     }//GEN-LAST:event_jMenuItem2ActionPerformed
 
         public static void main(String args[]) {
